9ed7378bc1c8dddbe4abbaa64187a30a29dde8dd
Pierre-Julien Villoud <pjvilloud@protonmail.com>
2019-03-14 15:34:04 +0100

    ajout tests annee anciennete

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,4 +1,64 @@
 package com.ipiecoles.java.java350.model;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 public class EmployeTest {
+
+    @Test
+    public void testGetNombreAnneeAncienneteNow(){
+        //Given
+        Employe employe = new Employe();
+        LocalDate dateEmbauche = LocalDate.now();
+        employe.setDateEmbauche(dateEmbauche);
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNull(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(null);
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNmoins2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().minusYears(2));
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteNplus2(){
+        //Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        //When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+
 }
